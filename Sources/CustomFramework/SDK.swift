41a1f602e390bcdce9324310a4314d0352a78dc2
Deric Kramer <deric.kramer@icanbwell.com>
2024-07-11 16:14:15 -0600

    first commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by deric kramer on 7/11/24.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomeWork() {
+        print("Doing some work in the SDK!")
+    }
+}
